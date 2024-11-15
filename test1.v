85856ab2707ab2d7da2ba44b5c9b781c543d0a95
amirali511 <amiralivatankhah3@gmail.com>
2024-11-15 18:10:57 +0330

    added .mod and test and core file

---
+++ test1.v
@@ -0,0 +1,10 @@
+import vastro
+
+fn main() {
+	println(vastro.solar_mass)
+	println(vastro.solar_radius)
+	println(vastro.solar_luminosity)
+	println(vastro.solar_temperature)
+	println(vastro.solar_apparent_magnitude)
+	println(vastro.solar_absolute_magnitude)
+}
---
+++ v.mod
@@ -0,0 +1,6 @@
+Module {
+	name: 'vastro'
+	description: 'Vastro is a small module for astronomical and star calculations'
+	version: '0.0.0'
+	dependencies: []
+}
