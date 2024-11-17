ba8f0b2f7d969fa8cee967fee4a6aefdf3ba7e14
amirali511 <amiralivatankhah3@gmail.com>
2024-11-17 19:45:13 +0330

    Temp + constants

--- test1.v
+++ test1.v
@@ -1,14 +1,8 @@
 import vastro
 
 fn main() {
-	println(vastro.solar_mass)
-	println(vastro.solar_radius)
-	println(vastro.solar_luminosity)
-	println(vastro.solar_temperature)
-	println(vastro.solar_apparent_magnitude)
-	println(vastro.solar_absolute_magnitude)
-	println(vastro.planet_temperature(f64(1.5 * 10e11), 
+	println(vastro.planet_temperature(f64(1.5 * 1e11), 
 										 f64(vastro.solar_temperature), 
 												  f64(vastro.solar_radius), 
-											  0.15))
+											  0.15, 1.0))
 }
--- vastro/vastro.v
+++ vastro/vastro.v
@@ -2,6 +2,15 @@ module vastro
 import math
 
 // Planet temperature
-pub fn planet_temperature(distance_from_star f64, star_temperature f64, star_radius f64, planet_albedo f64) f64 {
-	return star_temperature * math.pow((star_radius / (2 * distance_from_star)), 0.5) * math.pow((1 - planet_albedo), 0.25)
+pub fn planet_temperature(
+	distance_from_star f64, 
+	star_temperature f64, 
+	star_radius f64, 
+	planet_albedo f64,
+	atmospherical_constant f64
+) f64 {
+	return star_temperature * 
+				 math.pow((star_radius / (2.0 * distance_from_star)), 0.5) * 
+				 math.pow((1.0 - planet_albedo), 0.25) * atmospherical_constant
+
 }
