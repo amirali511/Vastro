81fde05d658ab3f790d49187ef30289aba46639c
Amirali Rahmani Vatan Khah <amiralivatankhah3@gmail.com>
2024-12-15 11:55:07 +0330

    Update test1.v

--- test1.v
+++ test1.v
@@ -2,9 +2,9 @@ import vastro
 
 fn main() {
 	println(vastro.planet_temperature(vastro.au, 
-										 f64(vastro.solar_temperature), 
-												  f64(vastro.solar_radius), 
-											  vastro.earth_albedo, 1.0))
+										 								f64(vastro.solar_temperature), 
+												  					f64(vastro.solar_radius), 
+											  						vastro.earth_albedo, 1.0))
 	
 	println(vastro.orbiting_time_in_seconds(vastro.au, vastro.solar_mass, vastro.earth_mass))
 	println(vastro.orbiting_time_in_days(vastro.au, vastro.solar_mass, vastro.earth_mass))
